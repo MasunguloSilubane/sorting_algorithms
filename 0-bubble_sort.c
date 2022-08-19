29aa894f71eb61ee8b8fff006a0fc19764660429
MasunguloSilubane <masungulogodi3@gmail.com>
2022-08-19 03:20:32 -0700

    Bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,33 +1,34 @@
 #include "sort.h"
+
 /**
- *bubble_sort - Function that sorts an array of ints
- *using bubble sort algorithm
- *
- *@array: array of ints
- *@size: size of array
- *
- */
+ *   * bubble_sort - Sorts an array of integers in ascending
+ *     * order using the Bubble sort algorithm.
+ *       * @array: The array of integers to sort
+ *         * @size: The size of the array of integers
+ *           *
+ *             * Return: Nothing!
+ *               */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-	size_t i, k;
+		int i = 0, aux = 0, max = 0, swapped = 1;
 
-	if (!array || !size)
-		return;
+			if (!array || size < 2)
+						return;
 
-	i = 0;
-	while (i < size)
-	{
-		for (k = 0; k < size - 1; k++)
-		{
-			if (array[k] > array[k + 1])
-			{
-				temp = array[k];
-				array[k] = array[k + 1];
-				array[k + 1] = temp;
-				print_array(array, size);
-			}
-		}
-		i++;
-	}
+				max = size - 1;
+
+					for (; i < max; ++i)
+							{
+										if (array[i] > array[i + 1])
+													{
+																	aux = array[i];
+																				array[i] = array[i + 1];
+																							array[i + 1] = aux;
+																										swapped = 1;
+																													print_array(array, size);
+																															}
+
+												if (swapped == 1 && i == max - 1)
+																i = -1, swapped = 0, --max;
+													}
 }
